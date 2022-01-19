3fcddaae98e3e08809cacd993f34eefb1b9b96b1
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2022-01-19 22:32:18 +0530

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -1,25 +1,57 @@
-class Queue:
-    def __init__(self, queue = []):
-        self.queue = queue
+class queue:
+    def __init__(self, size):
+        self.front = 0
+        self.rear = 0
+        self.queue = [None] * size
+        self.size = size + 1
         
-    def enqueue(self, queue, value):
-        self.queue += [value]
+    def current_size(self):
+        print(self.rear - self.front)
         
-    def dequeue(self, queue):
-        self.queue = self.queue[1:] 
+    def isempty(self):
+        if self.rear - self.front == 0:
+            print("True")
+        else:
+            print("False")
+            
+    def first(self):
+        print(self.queue[self.front])
     
-    def display(self, queue):
-        print(self.queue)
+    def enqueue(self, value):
+        if self.rear == self.size - 1:
+            print("Queue is Full")
+        elif self.rear == 0:
+            self.queue[self.rear] = value
+            self.rear += 1
+        else:
+            self.queue[self.rear] = value
+            self.rear = (self.rear + 1) % self.size
+            
+    def dequeue(self):
+        if self.front == self.size - 1:
+            print("Queue is Empty")
+        else:
+            self.queue[self.front] = None
+            self.front += 1
+            
+    def display(self):
+        for i in range(self.front, self.rear - 1):
+            print(self.queue[i], end = " ")
+        print(self.queue[self.rear - 1])
         
-        
-    
-q = Queue()
-queue = []
-q.enqueue(queue, 0)
-q.enqueue(queue, 1)
-q.enqueue(queue, 2)
-q.enqueue(queue, 3)
-q.display(queue)
-q.dequeue(queue)
-q.dequeue(queue)
-q.display(queue)
+ 
+size = int(input("Enter the size of the Queue:- "))
+q = queue(size)
+q.enqueue(0)
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.display()
+q.enqueue(5)
+q.dequeue()
+q.display()
+q.dequeue()
+q.first()
+q.dequeue()
+q.display()
